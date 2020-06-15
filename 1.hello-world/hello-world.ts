27edbf7026229f3973ecd5eeea4c6535cc4014a1
kenneth.cheung <kenneth.cheung@linecorp.com>
2020-06-15 15:37:34 +0800

    update

--- 1.hello-world/hello-world.ts
+++ 1.hello-world/hello-world.ts
@@ -1,1 +1,3 @@
+// I also can be run remotely
+// https://raw.githubusercontent.com/kenneth1003/deno-intro/master/1.hello-world/hello-world.ts
 console.log("Welcome to Deno 🦕")
--- 3.test-runner/add.test.ts
+++ 3.test-runner/add.test.ts
@@ -3,6 +3,6 @@ import minus from './add.ts'
 
 Deno.test("2 + 1 should be 3", () => {
   assertEquals(
-    minus(2, 1), 3)
-  
+    minus(2, 1), 3
+  )
 })
